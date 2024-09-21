36118c0759979716ad9921381f8e25323a847051
Ahire Tersoo David <tersooahire21@gmail.com>
2024-09-21 21:59:37 +0100

    created table

---
+++ project/src/Components/TableComponent/Table.stories.tsx
@@ -0,0 +1,15 @@
+import { Meta, StoryFn } from "@storybook/react";
+import { Table, TableType } from "./Table";
+
+export default {
+  title: "Components/Table",
+  component: Table,
+  decorators: [(Story) => <Story />],
+} as Meta;
+
+const TableTemplate: StoryFn<TableType> = (args) => {
+  return <Table {...args} />;
+};
+
+export const table = TableTemplate.bind({});
+table.args = {};
---
+++ project/src/Components/TableComponent/Table.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export type TableType = {
+  name: string;
+};
+
+export const Table = (props: TableType) => {
+  return <div>Table</div>;
+};
