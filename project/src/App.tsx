fa01bb9fa5496c5b2248dcae64057e4e22469810
Bluebberies <francisalexander000@gmail.com>
2024-09-26 20:09:55 +0100

    sync

--- project/src/App.tsx
+++ project/src/App.tsx
@@ -1,16 +1,12 @@
-// App.tsx
-import React from 'react';
-
-import { FaPlus, FaTrash, FaEdit } from 'react-icons/fa';
-import ActionButton from './Components/ActionButtonComponent/ActionButton';
+import { FaPlus } from "react-icons/fa";
+import ActionButton from "./Components/ActionButtonComponent/ActionButton";
 
 function App() {
-  const handleClick = () => alert('Button clicked!');
+  const handleClick = () => alert("Button clicked!");
 
   return (
     <div className="p-4 space-y-4 ">
       <ActionButton label="New Email" icon={<FaPlus />} onClick={handleClick} />
-      
     </div>
   );
 }
