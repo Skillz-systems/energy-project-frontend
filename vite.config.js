4575e0243606a18b030279010da0fbb5162292b3
kingsley achumie <kingsonly13c@gmail.com>
2025-04-02 21:26:10 +0100

    wip

--- vite.config.js
+++ vite.config.js
@@ -5,25 +5,30 @@ import tailwindcss from "tailwindcss";
 import tsconfigPaths from "vite-tsconfig-paths";
 // https://vitejs.dev/config/
 export default defineConfig({
-    server: {
-        host: "0.0.0.0",
-        port: 5173,
-        watch: {
-            usePolling: true,
-        },
+  server: {
+    historyApiFallback: true, // Helps with SPA routing
+    host: "0.0.0.0",
+    port: 5173,
+    watch: {
+      usePolling: true,
     },
-    plugins: [react(), tsconfigPaths()],
-    resolve: {
-        alias: {
-            "@": resolve(__dirname, "src"),
-        },
+  },
+  plugins: [react(), tsconfigPaths()],
+  base: "/", // Ensure the base is correctly set
+  build: {
+    outDir: "dist", // Ensure Vite builds into 'dist'
+  },
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "src"),
     },
-    optimizeDeps: {
-        include: ["@axios", "@react-icons"],
+  },
+  optimizeDeps: {
+    include: ["@axios", "@react-icons"],
+  },
+  css: {
+    postcss: {
+      plugins: [tailwindcss()],
     },
-    css: {
-        postcss: {
-            plugins: [tailwindcss()],
-        },
-    },
+  },
 });
